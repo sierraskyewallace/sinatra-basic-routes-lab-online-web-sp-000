ff259e0315bae914f3930a31c93e4dd11a867a41
Sierra Wallace <sskwallace@gmail.com>
2021-01-17 23:14:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,20 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
+   get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Skye"
+  end
+
+  get '/hometown' do
+    "My hometown is Lewes Beach"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Sludge Factory'"
+  end
   
 end
